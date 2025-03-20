1295ece4837e46ee8af0cf0c06fbdc21d4d19111
blueberrymann <2778511653@qq.com>
2025-03-20 22:58:56 +0800

    修改了一些参数

--- ptuning/train.sh
+++ ptuning/train.sh
@@ -10,12 +10,12 @@ torchrun --standalone --nnodes=1 --nproc-per-node=$NUM_GPUS main.py \
     --prompt_column content \
     --response_column summary \
     --overwrite_cache \
-    --model_name_or_path THUDM/chatglm2-6b \
+    --model_name_or_path /root/chatglm2-6b \
     --output_dir output/adgen-chatglm2-6b-pt-$PRE_SEQ_LEN-$LR \
     --overwrite_output_dir \
     --max_source_length 64 \
     --max_target_length 128 \
-    --per_device_train_batch_size 1 \
+    --per_device_train_batch_size 64 \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 16 \
     --predict_with_generate \
